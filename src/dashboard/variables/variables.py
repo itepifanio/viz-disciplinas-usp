651cc98402fbff70a2541ea38405ea6ad39fe926
Didiles <135085667+Didiles@users.noreply.github.com>
2025-10-30 23:27:11 -0300

    adicionando transformations e variables importantes

---
+++ src/dashboard/variables/variables.py
@@ -0,0 +1,4 @@
+obrigatorias = ['SCC5763', 'SCC5832', 'SCC5900', 'SME5781', 'SME5901', 'SME5902', 'SSC5723'] # disciplinas obrigatórias, permite escolha
+blocker = 'SCC5933' # metodologia científica, não permite escolha
+creditos_necessarios = 50 # créditos totais necessários para a conclusão do mestrado
+creditos_obrigatorios = 12 # créditos de disciplinas obrigatorias necessários para a conclusão do mestrado, créditos de metodologia já descontado
