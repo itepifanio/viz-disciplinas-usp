2f56c2923542031f610d0e7bd0fcecf0ecd973ed
Ítalo Epifânio <italo.epifanio@shapedigital.com>
2025-10-31 23:04:02 -0300

    Finaliza scraper de acordo com as especificacoes

--- src/scraper/disciplinas/spiders/janus_disciplinas_spider.py
+++ src/scraper/disciplinas/spiders/janus_disciplinas_spider.py
@@ -135,7 +135,7 @@ class JanusDisciplinasSpider(Spider):
         nome_programa: str,
         codigo_area_concentracao: str,
         nome_area_concentracao: str
-    ) -> Generator[dict, None, None]:
+    ) -> Generator[Request, None, None]:
         """
         Extrai as disciplinas da área de concentração específica.
         """
@@ -168,15 +168,149 @@ class JanusDisciplinasSpider(Spider):
                 turma_url: {turma_url}
             """)
 
-            yield {
-                'codigo_comissao': codigo_comissao,
-                'nome_comissao': nome_comissao,
-                'codigo_programa': codigo_programa,
-                'nome_programa': nome_programa,
-                'codigo_area_concentracao': codigo_area_concentracao,
-                'nome_area_concentracao': nome_area_concentracao,
-                'codigo_disciplina': codigo_disciplina,
-                'nome_disciplina': nome_disciplina,
-                'ementa_url': ementa_url,
-                'turma_url': turma_url
-            }
+            yield Request(
+                    url=(
+                        'https://uspdigital.usp.br/janus/Disciplina?sgldis='
+                        f'{codigo_disciplina}&'
+                    ),
+                    callback=self.parse_ementa,
+                    cb_kwargs={
+                        'codigo_comissao': codigo_comissao,
+                        'nome_comissao': nome_comissao,
+                        'codigo_programa': codigo_programa,
+                        'nome_programa': nome_programa,
+                        'codigo_area_concentracao': codigo_area_concentracao,
+                        'nome_area_concentracao': nome_area_concentracao,
+                        'codigo_disciplina': codigo_disciplina,
+                        'nome_disciplina': nome_disciplina,
+                        'ementa_url': ementa_url,
+                        'turma_url': turma_url
+                    }
+                )
+
+    def _get_text_after_strong_label(self, response: Response, label_text: str) -> str:
+        """
+        Função auxiliar para pegar o texto de um <p class="...campoTextoMantendoLinhas">
+        que vem logo após um <p> contendo um <strong> com o 'label_text'.
+        """
+        text_nodes = response.xpath(
+            f"//strong[text() = '{label_text}']/parent::p/following-sibling::p"
+            "[contains(@class, 'campoTextoMantendoLinhas')][1]//text()"
+        ).getall()
+
+        return "".join(text_nodes).strip()
+
+
+    def parse_ementa(
+        self,
+        response: Response,
+        codigo_comissao: str,
+        nome_comissao: str,
+        codigo_programa: str,
+        nome_programa: str,
+        codigo_area_concentracao: str,
+        nome_area_concentracao: str,
+        codigo_disciplina: str,
+        nome_disciplina: str,
+        ementa_url: str,
+        turma_url: str
+    ) -> Generator[dict, None, None]:
+        """
+        Extrai a ementa da disciplina.
+        """
+        ementa_criacao: str | None = None
+        numero_creditos: int | None = None
+        carga_horaria_teorica: int | None = None
+        carga_horaria_pratica: int | None = None
+        carga_horaria_estudo: int | None = None
+        carga_horaria_duracao: str | None = None
+        carga_horaria_total: str | None = None
+        docentes_responsaveis: str | None = None
+        ementa_objetivos: str | None = None
+        ementa_justificativa: str | None = None
+        ementa_conteudo: str | None = None
+        ementa_forma_avaliacao: str | None = None
+        ementa_bibliografia: str | None = None
+        ementa_idiomas: str | None = None
+        ementa_tipo_oferecimentos: str | None = None
+
+        try:
+            (
+                ch_teorica,
+                ch_pratica,
+                ch_estudo,
+                carga_horaria_duracao,
+                _,
+                carga_horaria_total,
+                _
+            ) = response.css("table.dataTable.selecionavel > tr td::text").getall()
+        except Exception:
+            logger.error(
+                f"Erro ao extrair cargas horárias para a disciplina: "
+                f"{codigo_disciplina} - {nome_disciplina}"
+                f"foi extraido {response.css('table.dataTable.selecionavel > tr td::text').getall()}"
+            )
+
+        try:
+            carga_horaria_teorica = int(ch_teorica.strip())
+            carga_horaria_pratica = int(ch_pratica.strip())
+            carga_horaria_estudo = int(ch_estudo.strip())
+        except ValueError:
+            carga_horaria_teorica = None
+            carga_horaria_pratica = None
+            carga_horaria_estudo = None
+
+        p_elements = response.css("p.info.infopt")
+
+        for p_element in p_elements:
+            topico = p_element.css("strong::text").get(default='').strip()
+
+            if topico == "Criação:":
+                ementa_criacao = p_element.css("::text").get(default='').strip()
+            elif topico == "Nº de Créditos:":
+                numero_creditos_text = p_element.css("::text").get(default='').strip()
+                try:
+                    numero_creditos = int(numero_creditos_text)
+                except ValueError:
+                    numero_creditos = None
+
+        docentes_nodes = response.xpath(
+            "//strong[text() = 'Docentes Responsáveis:']/parent::p/following-sibling"
+            "::p[@class='info' and not(strong)]"
+        )
+        docentes_responsaveis = ' | '.join([
+            node.css('::text').get(default='').strip() for node in docentes_nodes
+        ])
+
+        ementa_idiomas = ' | '.join(
+            response.xpath(
+                "//strong[text() = 'Idiomas ministrados:']/parent::p/"
+                "following-sibling::div/text()"
+            ).getall()
+        )
+        ementa_conteudo = self._get_text_after_strong_label(response, "Conteúdo:")
+        ementa_objetivos = self._get_text_after_strong_label(response, "Objetivos:")
+        ementa_justificativa = self._get_text_after_strong_label(response, "Justificativa:")
+        ementa_forma_avaliacao = self._get_text_after_strong_label(response, "Forma de Avaliação:")
+        ementa_bibliografia = self._get_text_after_strong_label(response, "Bibliografia:")
+        ementa_tipo_oferecimentos = self._get_text_after_strong_label(response, "Tipo de oferecimento da disciplina:")
+
+        yield {
+            'codigo': codigo_disciplina,
+            'disciplina': nome_disciplina,
+            'criacao': ementa_criacao,
+            'n_creditos': numero_creditos,
+            'carga_teorica': carga_horaria_teorica,
+            'carga_pratica': carga_horaria_pratica,
+            'carga_estudo': carga_horaria_estudo,
+            'duracao': carga_horaria_duracao,
+            'carga_total': carga_horaria_total, # adicionei essa fora da especificação
+            'docentes_responsaveis': docentes_responsaveis,
+            'objetivos': ementa_objetivos,
+            'justificativa': ementa_justificativa,
+            'avaliacao': ementa_forma_avaliacao,
+            'conteudo': ementa_conteudo,
+            'bibliografia': ementa_bibliografia,
+            'idioma': ementa_idiomas,
+            'oferecimento': ementa_tipo_oferecimentos,
+        }
